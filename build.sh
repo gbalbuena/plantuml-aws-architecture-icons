b903486de325c59583c4a4c99bffd0efca1c6c44
Juan Gabriel Balbuena Borba <gabrielbalbuena@gmail.com>
2019-05-27 12:38:32 +1000

    Update icons

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-find $(pwd) -type d -print0 | 
+find "$(pwd)/AWS_Architecture_Icons_PNG" -type d -print0 | 
   while IFS= read -r -d '' d; do 
     find $d -depth -name '*-*.png' \
       -execdir bash -c 'mv -- "$1" "${1//-/_}"' bash {} \;
   done
 
-find $(pwd) -type f -name '*.png' -print0 | 
-  while IFS= read -r -d '' i; do 
-    java -jar plantuml.1.2019.5.jar -encodesprite 16z $i > "${i%.*}.puml"
+find "$(pwd)/AWS_Architecture_Icons_PNG" -type f -name '*.png' -print0 | 
+  while IFS= read -r -d '' i; do
+    if [ -f "${i%.*}.puml" ]; then
+      echo "$i already exist."
+    else 
+      java -jar plantuml.1.2019.5.jar -encodesprite 16z "$i" > "${i%.*}.puml"
+    fi
   done
 
 total_png=$(find $(pwd) -type f -name '*.png' | wc -l)
